85ca9ce987435290fa2f9b64eab456dc2d1327d8
Ali-Raza-H <dannygamer.ali@gmail.com>
2025-09-16 21:51:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 async function humanize() {
     const input = document.getElementById("inputText").value;
 
-    const response = await fetch("https://humanizer-2qzw.onrender.com", {
+    const response = await fetch("http-client/2.0", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -18,4 +18,5 @@ async function humanize() {
     document.getElementById("result").innerHTML =
         "<h3>Humanized Text:</h3><p>" + data.humanized_text + "</p>";
 }
+
 
